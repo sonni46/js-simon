f0bef538e04b1e3b22e7888dd8c3fa14d50a29c4
Marco <sonni46marcosenpai@gmail.com>
2021-07-29 15:20:28 +0200

    update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,25 @@
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+
+let numeri = [];
+let volte = 5;
+
+numGen();
+console.log(numeri)
+
+// function// function// function// function// function// function
+
+function numGen() {
+    for (let i = 0; i < volte;i++){
+        let numRand;
+        do {
+            numRand = randomico(50);
+        }while(numeri.includes(numRand))
+        numeri.push(numRand)
+    } 
+}
+
+function randomico (max){
+    return Math.floor(Math.random() * max);
+};
